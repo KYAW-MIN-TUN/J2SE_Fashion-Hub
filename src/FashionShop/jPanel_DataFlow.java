852908508cb25f2778c89a78ce6796258a9df7a0
DESKTOP-G3QTI2K\User <kyawmintun10498@gmail.com>
2018-06-03 03:34:43 +0630

    Initial Project

---
+++ src/FashionShop/jPanel_DataFlow.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FashionShop;
+
+import java.awt.GridLayout;
+
+/**
+ *
+ * @author User
+ */
+class jPanel_DataFlow {
+    
+    static void setLayout(GridLayout gridLayout) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void add(PanelView panel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void removeAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
